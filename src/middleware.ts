5c8a706c70cc91b7bc8de447c6600a18ff0bcc48
msajiba <asajib499@gmail.com>
2025-03-02 12:17:08 +0600

    Update middleware path matcher to include attendance route

--- src/middleware.ts
+++ src/middleware.ts
@@ -24,5 +24,5 @@ export async function middleware(request: NextRequest) {
 
 // See "Matching Paths" below to learn more
 export const config = {
-  matcher: ["/", "/employees", "/attendance-list"],
+  matcher: ["/", "/employees", "/attendance", "/attendance-list"],
 };
